46a5867ae3069f24abab7eb57f196e6986ef3b07
Samsoucy <newf930@outlook.com>
2019-03-04 16:56:31 -0500

    added a get to allow logged in users to see user list

--- index.js
+++ index.js
@@ -48,7 +48,40 @@ server.post('/api/login', (req, res) => {
       .catch(error => {
         res.status(500).json(error);
       });
-  });
+});
+
+//************User logged in to see list of users *******************
+
+function restricted(req, res, next) {
+    const { username, password } = req.body
+    
+    if (username && password) {
+        Users.findBy({ username })
+            .first()
+            .then(user => {
+                if (user && bcrypt.compareSync(password, user.password)) {
+                    next();
+                } else {
+                    res.status(401).json({message:"You shall not pass!"})
+            }
+            })
+            .catch(error => {
+            res.status(500).json(error)
+        })
+    } else {
+        res.status(400).json({message:"must provide credentials"})
+    }
+}
+
+server.get('/api/users', restricted, (req, res) => {
+    Users.find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(err => res.send(err));
+})
+  
+
   
 
 
--- knexfile.js
+++ knexfile.js
@@ -15,8 +15,5 @@ module.exports = {
     migrations: {
       directory: './data/migrations',
     },
-    seeds: {
-      directory: './data/seeds',
-    },
   },
 };
